276b686aec17b78907ba50856720123d50852efd
Sarah Braga <sara.bragga01@gmail.com>
2023-02-16 19:20:45 -0300

    Adição da divisão da pipeline em silver e bronze

--- extract-files-monthly.py
+++ extract-files-monthly.py
@@ -1,20 +1,36 @@
 # Importação de bibliotecas
-from datetime import datetime, timedelta
+import datetime
+from datetime import timedelta
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from airflow.contrib.operators.bigquery_operator import BigQueryOperator
 from Class import Class_coleta as coleta
 from Class import Class_validacao as valida
 from Class import Class_persiste as persiste
+from dateutil.relativedelta import relativedelta
+
 
 # Variáveis
 default_args = {
     'owner': 'sarah braga',
-    'retries': 1,
+    'retries': 0,
     'retry_delay': 0,
-    'start_date': datetime(2023, 2, 15),
+    'start_date': datetime.datetime(2023, 2, 15),
     'execution_timeout': timedelta(seconds=120)
 }
+
+# Calcula o ano e mês que os dados devem ser extraídos
+data = datetime.date.today() - relativedelta(months=+2)
+mes = data.month
+ano = data.year
+
+# Caso o mês esteja entre janeiro - setembro, acrescenta o 0 na frente do número
+if mes >=1 and mes <= 9:
+    mes = '0' + str(mes)
+
+busca = str(ano) + str(mes)
 
 def create_task(name, function, kwargs, dag):
     return PythonOperator(
@@ -49,17 +65,53 @@ with DAG(
         # Coleta os dados de 5 meses atrás na API
         coletaDadosMes3 = create_task("coletaDadosMes3", coleta.Coleta.coleta_dados, {"meses": 5}, dag)
 
-        # Dummy para marcar o final da dag
-        finish = DummyOperator(task_id='finish')
-
         # Valida o retorno da API
         validaDadosRetornoAPI = create_task("validaDadosRetornoAPI", valida.ValidaDados.retorno_api, {}, dag)
 
         # Verifica se os dados podem ser convertidos para json
         validaDadosVerificaJSON = create_task("validaDadosVerificaJSON", valida.ValidaDados.verifica_json, {}, dag)
 
-        # Cria e separa os dataframes 
-        persisteDados = create_task("persisteDados", persiste.PersisteDados.main, {}, dag)
+        # Insere os dados na camada bronze
+        insereBronze = create_task("insereBronze", persiste.PersisteDados.insereBronze, {}, dag)
+
+        # Insere os dados na camada silver
+        insereSilverPrescriptions = BigQueryOperator(
+             task_id = 'insereSilverPrescriptions'
+            ,sql = '\
+                SELECT  \
+                    * \
+                FROM \
+                    `bronze.TB_ANALITICO_PRESCRIPTIONS`\
+                WHERE \
+                    YEAR_MONTH > ' + busca
+            , destination_dataset_table = 'silver.TB_PRESCRIPTIONS'
+            ,use_legacy_sql = False
+            ,write_disposition = 'WRITE_APPEND' # se a tabela já existir, insere os dados
+            ,dag=dag
+        )
+
+        # Insere os dados na camada silver
+        insereSilverPrescribers = BigQueryInsertJobOperator(
+             task_id = 'insereSilverPrescribers'
+            ,configuration = {
+              'query': {
+                    'query': '\
+                        MERGE silver.TB_PRESCRIBERS silver \
+                        USING (SELECT DISTINCT PRACTICE_CODE,PRACTICE_NAME FROM bronze.TB_ANALITICO_PRESCRIPTIONS) bronze \
+                        ON bronze.PRACTICE_CODE = silver.PRACTICE_CODE \
+                        WHEN MATCHED THEN \
+                        UPDATE SET silver.PRACTICE_NAME = bronze.PRACTICE_NAME, silver.PRACTICE_CODE = bronze.PRACTICE_CODE  \
+                        WHEN NOT MATCHED THEN \
+                        INSERT (PRACTICE_NAME, PRACTICE_CODE) VALUES (PRACTICE_NAME, PRACTICE_CODE)'
+                    ,'use_legacy_sql': False
+                } 
+            }
+            ,project_id = 'molten-complex-377821'
+            ,dag=dag
+        )
+
+        # Dummy para marcar o final da dag
+        finish = DummyOperator(task_id='finish')
 
 # Fluxo de execução das jobs
-init >> [coletaDadosMes1, coletaDadosMes2, coletaDadosMes3] >> validaDadosRetornoAPI >> validaDadosVerificaJSON >> persisteDados >> finish
+init >> [coletaDadosMes1, coletaDadosMes2, coletaDadosMes3] >> validaDadosRetornoAPI >> validaDadosVerificaJSON >> insereBronze >> insereSilverPrescriptions >> insereSilverPrescribers >> finish
